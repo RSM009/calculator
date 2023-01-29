ab02541c6ce13bb25da2364d8edc8a49bbf37685
Ravi Shankar Meena <107501908+RSM009@users.noreply.github.com>
2023-01-29 09:45:21 +0530

    first comit

---
+++ calcultion.py
@@ -0,0 +1,38 @@
+import matplotlib.pyplot as plt
+import colorama
+from colorama import Fore
+print(Fore.RED +"............................Welcome you to Compound Interest calculator......................................")
+starting_bal = int(input(Fore.GREEN +"Starting Balance :- "))
+#monthly_contri =int(input   ("Monthly Contribution(optional):- "))
+intrest_rate=int( input(Fore.CYAN +"Interest Rate :- "))
+duration = int(input(Fore.YELLOW +"Duration :- "))
+principal_graph = []
+
+def compounding(sb,ir,du):
+    principal=0
+    principal = sb 
+    for i in range(du):
+        interest=  (principal)*ir/100
+        interest = round(interest,2)
+        principal = principal + interest
+        principal =round(principal, 2)
+        principal_graph.append(principal)
+        print(Fore.GREEN+"interest:- ",interest,Fore.CYAN+ "  principal:- ",principal)
+        
+    return principal
+time_du = []
+for i in range(duration):
+    time_du.append(i+1)
+result = compounding(starting_bal,intrest_rate,duration)          
+print(Fore.MAGENTA+"total :- ",result) 
+
+plt.bar(time_du, principal_graph,color='green')
+
+for a,b in zip(time_du, principal_graph): 
+    plt.text(a, b, str(b))
+
+plt.title("calculation Plot", fontsize=28)
+plt.ylabel("principal", fontsize=18)
+
+plt.xlabel("time", fontsize=18)
+plt.show()  
---
+++ date_difference_calculator.py
@@ -0,0 +1,5 @@
+import colorama
+from colorama import Fore
+
+print(Fore.RED +"............................Welcome you to Date/differnce calculator......................................")
+print(Fore.GREEN +"............................Welcome you to Date/differnce calculator......................................")
